32db4aa8df60401b256a27bef9647e1296d78178
Guillaume Mantopoulos <3082385+MrGuiMan@users.noreply.github.com>
2023-11-14 13:00:09 +0100

    add clangd to lsp servers to install

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,9 +1,10 @@
 local lsp = require('lsp-zero').preset('recommended')
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'rust_analyzer',
-	'ruby_ls'
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+    'ruby_ls',
+    'clangd'
 })
 -- (Optional) Configure lua language server for neovim
 -- lsp.nvim_workspace()
