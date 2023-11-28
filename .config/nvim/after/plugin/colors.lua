5cb8e869a0b018554168ddef70c39475c7af0bec
Guillaume Mantopoulos <3082385+MrGuiMan@users.noreply.github.com>
2023-11-28 18:30:42 +0100

    nvim config v2
    - simplified file structure
    - Fixed clipboard yanking
    - replaced Packer with lazy
    - removed lsp zero
    - manual configuration of completion plugins
    - manual configuration of lsp plugins
    - replaced nvim-tree with telescope file browser

--- .config/nvim/after/plugin/colors.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd.colorscheme("zenburn")
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- .config/nvim/lua/jahz/remap.lua
+++
@@ -1,5 +0,0 @@
--- Move Highlighted Lines in normal and visual mode
-vim.keymap.set("n", "J", ":m .+1<CR>==")
-vim.keymap.set("n", "K", ":m .-2<CR>==")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
